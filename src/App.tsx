c459fc6b2be002ac2f4ba3689ba934f9fec5dda1
Katharina Sick <katharina.sick@hotmail.com>
2022-12-15 19:07:11 +0100

    fix: Remove upload dependency

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Menu from "./components/Menu";
 import Portfolio from "./components/Portfolio";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Upload from "./components/Upload";
 import NotFound from "./components/NotFound";
 import Footer from "./components/Footer";
 
@@ -19,7 +18,7 @@ const App = () => (
                     <Route path={"/"} element={<Portfolio/>}/>
                     <Route path={"/about"} element={<About/>}/>
                     <Route path={"/contact"} element={<Contact/>}/>
-                    <Route path={"/upload"} element={<Upload/>}/>
+                    {/*<Route path={"/upload"} element={<Upload/>}/>*/}
                     <Route path={"*"} element={<NotFound/>}/>
                 </Routes>
             </div>
